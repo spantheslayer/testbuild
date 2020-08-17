8f6643acb11c5681212aca4d5afaa5658310ea2f
spantheslayer <supriyosarkar282@gmail.com>
2020-08-17 22:15:27 +0530

    first se7

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import {StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  StatusBar,
+} from 'react-native';
 
 export default function WelcomeScreen() {
   return (
-    <View>
-      <Image source={require('../assets/SaveITtext.png')} />
-      <Text></Text>
-    </View>
+    <>
+      <StatusBar hidden={true} />
+      <ImageBackground
+        style={styles.imageBackground}
+        source={require('../assets/mola.png')}>
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/SaveITtext.png')} />
+          <Text style={{fontFamily: 'FlashbackDemo', fontSize: 40}}>
+            Save your notes acroos all your devices{' '}
+          </Text>
+        </View>
+      </ImageBackground>
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageBackground: {
+    flex: 1,
+    backgroundColor: '#FF1E56',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 33,
+    alignItems: 'center',
+  },
+});
