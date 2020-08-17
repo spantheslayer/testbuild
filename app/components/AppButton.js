4556046548b8eb4b173212e50a2d5e4f3dce42a4
spantheslayer <supriyosarkar282@gmail.com>
2020-08-17 21:37:46 +0530

    added reusable button component

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -1,2 +1,28 @@
 import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import Colors from '../config/Colors';
+
+export default function AppButton() {
+  return (
+    <TouchableOpacity style={[styles.buttons, {backgroundColor: Color[color]}]}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttons: {
+    backgroundColor: Colors.primary,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    width: '100%',
+    marginVertical: 7,
+  },
+  text: {
+    color: Colors.white,
+    fontSize: 15,
+    textTransform: 'uppercase',
+  },
+});
