86cf1f457c5f56515ec59bbf4f4953efdc734d49
spantheslayer <supriyosarkar282@gmail.com>
2020-08-19 02:20:38 +0530

    On morning

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,5 +1,6 @@
 export default {
   background: '#222831',
+  primary: '#393e46',
   secondary: '#32e0c4',
   black: '#000',
   white: '#fff',
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import colors from '../config/colors';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default function WelcomeScreen() {
   return (
     <View style={styles.background}>
       <Text style={styles.text}>SaveIt</Text>
+      <Text style={styles.slogan}>Save your notes across all your devices</Text>
+      <TouchableOpacity style={styles.button}>
+        <LinearGradient colors={['#393e46', '#32e0c4']} style={styles.login}>
+          <Text style={styles.logintext}>Login</Text>
+        </LinearGradient>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -21,5 +28,25 @@ const styles = StyleSheet.create({
     fontFamily: 'FlashbackDemo',
     fontSize: 150,
     color: colors.secondary,
+    bottom: 200,
+  },
+  slogan: {
+    fontFamily: 'Dance',
+    fontSize: 25,
+    color: colors.secondary,
+    bottom: 220,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80%',
+  },
+  login: {
+    borderRadius: 30,
+
+    padding: 15,
+  },
+  logintext: {
+    color: colors.white,
   },
 });
